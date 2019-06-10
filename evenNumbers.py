e2581cf85f27bdbd34b2a7ebe5cbfb4ffd19a41f
Dominic <domelgie94@aol.com>
2019-06-10 23:25:13 +0000

    Finished tests

--- evenNumbers.py
+++ evenNumbers.py
@@ -1,18 +1,10 @@
+def is_even(number):
+    return number % 2 == 0
+
 def even_number_of_evens(numbers):
-    if numbers == []:
-        return False
-    else:
-        evens = 0
-        
-        
-    for number in numbers:
-        if number % 2 == 0:
-            evens += 1
-            
-    if evens == 0:
-        return False
-    else:
-        return evens % 2 == 0
+    
+    evens = sum([1 for number in numbers if is_even(number)])
+    return False if evens == 0 else is_even(evens)
 
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2]) == False, "One even number"
@@ -23,16 +15,3 @@ assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple n
 assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
 print("All tests passed!")
-
-
-"""
-
-Write a function that accepts a list of numbers and returns True 
-if the list contains an even number of even numbers.
-
-If the list has no numbers or 0, return False.
-If the list has odd numbers, return False.
-If the list has a odd number of even numbers, return False.
-If the list has a even number of evens numbers, return True.
-
-"""
